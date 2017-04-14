c3c9e4da28ef1eba501855c1fc28f064d83b1808
Batista Ramos <batista@Batistas-MacBook-Pro.local>
2017-04-14 18:31:45 -0300

    tradução

--- protected/framework/gii/generators/form/templates/default/form.php
+++ protected/framework/gii/generators/form/templates/default/form.php
@@ -18,7 +18,7 @@
 	'enableAjaxValidation'=>false,
 )); ?>\n"; ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+	<p class="note">Os campos com<span class="required">*</span> são obrigatórios.</p>
 
 	<?php echo "<?php echo \$form->errorSummary(\$model); ?>\n"; ?>
 
